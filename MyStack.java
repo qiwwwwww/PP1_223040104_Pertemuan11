fbbe0b41a012e98964e890e7ef1e1613da5b9e81
qiwwwwww <124857174+qiwwwwww@users.noreply.github.com>
2024-05-12 23:38:30 +0700

    Add files via upload

---
+++ MyStack.java
@@ -0,0 +1,21 @@
+package Pertemuan11;
+
+public class MyStack extends AbstractStack {
+    public MyStack(int capacity) {
+        super(capacity);
+    }
+
+    public void displayStack(String s) {
+        System.out.println(s);
+        System.out.println("stack (bottom-->):");
+        for (int i = 0; i < size(); i++) {
+            System.out.print(peekN(i));
+            System.out.print(' ');
+        }
+        System.out.println("");
+    }
+
+    public int peekN(int n) {
+        return arr[n];
+    }
+}
